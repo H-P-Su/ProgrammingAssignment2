9b037a9c1555d7fdf79152506f240ef97ea4ae4a
Hua Su <xtals8hua@gmail.com>
2016-12-23 20:56:00 -0500

    Attempted comments for describing functions to be written

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# Based on the template, makeVector, this function should contain:
+# set the value of the matrix
+# get the value of the matrix
+# set the value of the inverse matrix
+# get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+# this function should see if the inverse has been set.
+# if the inverse has been set, it should get the inverse matrix
+# if the inverse has not been set, it should set the inverse matrix with the setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
