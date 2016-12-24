d7fce2a1846099ed433104b2e8803c842b8cab48
Hua Su <xtals8hua@gmail.com>
2016-12-23 22:10:18 -0500

    Coded both functions. Appears to work with test matrices.

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-# Based on the template, makeVector, this function should contain:
+# Based on the template, makeVector, this function should be a list containing a function to:
 # set the value of the matrix
 # get the value of the matrix
 # set the value of the inverse matrix
 # get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<- y
+  }
+  get <- function() x
+  setinverse <- function(inverse_matrix) i<<- inverse_matrix
+  getinverse <- function() i
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse
+  )
 }
 
 
 # this function should see if the inverse has been set.
 # if the inverse has been set, it should get the inverse matrix
 # if the inverse has not been set, it should set the inverse matrix with the setinverse function.
+# assuming the matrix supplied is always invertable
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)){
+    message("Getting cached inverse.")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
